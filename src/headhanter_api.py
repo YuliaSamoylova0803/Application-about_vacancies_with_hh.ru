c6331df14cc195448d9ce03f4a0f0e4059ca344c
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-02-04 20:56:06 +0500

    add get vacancies

--- src/headhanter_api.py
+++ src/headhanter_api.py
@@ -1,9 +1,15 @@
 from abc import ABC, abstractmethod
 
 class Parser(ABC):
-
+    """Абстрактный класс для работы с API сервиса с вакансиями"""
     @abstractmethod
     def load_vacancies(self):
+        pass
+
+class HeadHunterAPI(Parser):
+    pass
+
+    def get_vacancies(self):
         pass
 
 
