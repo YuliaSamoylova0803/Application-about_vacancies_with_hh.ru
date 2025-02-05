e38289a1438739362e5549c30ba1049cc8d01711
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-02-05 13:54:51 +0500

    update method load vacancies

--- src/headhanter_api.py
+++ src/headhanter_api.py
@@ -1,16 +1,39 @@
+import json
+import requests
 from abc import ABC, abstractmethod
 
 class Parser(ABC):
     """Абстрактный класс для работы с API сервиса с вакансиями"""
     @abstractmethod
-    def load_vacancies(self):
+    def _load_vacancies(self):
+        """Метод отправки get-запроса на сайт Head Hunter"""
         pass
 
 class HeadHunterAPI(Parser):
-    pass
+    """Класс для работы с API HeadHunter"""
 
-    def get_vacancies(self):
-        pass
+    def __init__(self):
+        """Магический метод инициализаций объектов для отправки get-запроса"""
+        self._url = "https://api.hh.ru/vacancies"
+        self._headers = {"User-Agent": "HH-User-Agent"}
+        self.params = {'text': '', 'page': 0, 'per_page': 100}
+        self.vacancies = []
+
+    def _load_vacancies(self, keyword):
+        """Метод отправки get-запроса на сайт Head Hunter"""
+        self.params["text"] = keyword
+        while self.params.get("page") != 20:
+            response = requests.get(self._url, headers=self._headers, params=self.params)
+            vacancies = response.json()["items"]
+            self.vacancies.extend(vacancies)
+            self.params["page"] += 1
+        return self.vacancies
 
 
+if __name__ == "__main__":
+    hh_api = HeadHunterAPI()
+    print(hh_api)
+    # Получение вакансий с hh.ru в формате JSON
+    hh_vacancies = hh_api._load_vacancies("Python")
+    print(hh_vacancies)
 
