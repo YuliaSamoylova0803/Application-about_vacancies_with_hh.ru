5353cd0b44491914f1c3900bf7013c4c984672a1
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-02-09 16:31:11 +0500

    update class JSONSaver

--- src/saver.py
+++ src/saver.py
@@ -1,4 +1,5 @@
 import json
+import os
 from abc import ABC, abstractmethod
 
 
@@ -6,7 +7,7 @@ class Saver(ABC):
     """Абстрактный класс, который обязывает реализовать методы для добавления вакансий в файл"""
 
     @abstractmethod
-    def add_vacancies_to_file(self):
+    def add_vacancies_to_file(self, vacancy):
         pass
 
     @abstractmethod
@@ -15,29 +16,28 @@ class Saver(ABC):
 
 class JSONSaver(Saver):
     """ Класс для работы с файлами (vacancy_list из класса HeadHunterAPI) """
-    def __init__(self, path: str = "data/vacancy_list_json.json"):
-        self.__path = path
-        self.info_about_vacancies = []
+    def __init__(self, filename):
+        self.filename = filename
 
     # блок функций для добавления в файлы
-    def add_vacancies_to_file(self, vacancies: list[dict]):
+    def add_vacancies_to_file(self, value):
         """ Функция добавляет данные формата json в файл"""
 
-        with open(self.__path, "w", encoding="utf-8") as json_file:
-            json.dump(vacancies, json_file, ensure_ascii=False, indent=4)
+        with open(self.filename, "a", encoding="utf-8") as file:
+            json.dump(value, file, indent=4, ensure_ascii=False)
+            file.write("\n")
 
-    # блок функций для чтения из файла
+        # блок функций для чтения из файла
 
     def read_data_json(self):
         """ Чтение json файла """
-        with open(self.__path, 'r', encoding='utf-8') as json_file:
-            data_from_hh = json.load(json_file)
-            return data_from_hh
-
+        with open(self.filename, 'r', encoding='utf-8') as file:
+            return json.load(file)
 
 
     def delete_vacancy(self):
-        pass
+        open(self.filename, 'w').close()
+        os.remove(self.filename)
 
 
 if __name__ == "__main__":
